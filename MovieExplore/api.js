21d2191cbede2236e425f1fee183546869bef383
Umino <umino.sharp@gmail.com>
2017-10-22 19:18:36 +0700

    Update api for  get movie detail

--- MovieExplore/api.js
+++ MovieExplore/api.js
@@ -1,5 +1,7 @@
 let rootURL = 'http://api.themoviedb.org/3/';
-//'https://api.themoviedb.org/3/search/movie?api_key=aa48129d026a4b12f9d9b14975098006&query=thor'
+//search: 'https://api.themoviedb.org/3/search/movie?api_key=[xxx]&query=[movie name]'
+//detail: 'http://api.themoviedb.org/3/movie/[movie id]?api_key=[xxx]'
+
 let api_key = 'aa48129d026a4b12f9d9b14975098006';
 exports.search = function(movieName) {
     let url = `${rootURL}search/movie?api_key=${api_key}&query=${movieName}`;
@@ -9,6 +11,8 @@ exports.search = function(movieName) {
     }));
 }
 
-// exports.view = function(id){
-//     let url = `${rootURL}`
-// }
+exports.detail = function(movieId){
+    let url = `${rootURL}movie/${movieId}?api_key=${api_key}`;
+    console.log(url);
+    return fetch(url).then((resp) => resp.json());
+}
