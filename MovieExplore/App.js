91841b34d488a51c1135bef959edaccbb1df47ba
Umino <umino.sharp@gmail.com>
2017-10-22 13:33:04 +0700

    Update MovieExplorer

---
+++ MovieExplore/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+   StyleSheet,
+   Text,
+   View,
+   StatusBar,
+  } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import MovieListScreen from './MovieListScreen';
+import MovieDetai from './MovieDetail';
+
+const Screens = StackNavigator({
+  homePage: { screen: MovieListScreen },
+  movieDetailPage: { screen: MovieDetai },
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar backgroundColor='firebrick'/>
+        <Screens />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
